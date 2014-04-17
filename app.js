d7768199d9019b680cdebcbe08372d8967be9c24
Nathan Bryan <nathan.bryan@gmail.com>
2014-04-17 01:13:07 -0400

    Get routing and templates working

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
 var express = require('express');
 
 var app = express();
-app.get('/hello', function(req, res) {
-  res.send("Hello, World!");
+app.set('view engine', 'jade');
+app.use(express.static('./assets'));
+
+app.locals.title = 'Shultis-Bryan Wedding';
+
+app.get('/', function(req, res) {
+  res.render('index');
+});
+app.get('/:page', function(req, res) {
+  res.render(req.params['page']);
 });
 
 app.listen(process.env.PORT || 3000);
