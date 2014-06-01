0055c48a970500df3fb2a8afd24729e9ab54d118
Nathan Bryan <nathan.bryan@gmail.com>
2014-06-01 00:56:03 -0400

    Home page and layout changes

--- app.js
+++ app.js
@@ -6,11 +6,15 @@ app.use(express.static('./assets'));
 
 app.locals.title = 'Shultis-Bryan Wedding';
 
+app.get('/favicon.ico', function(res, res) {
+  res.set('Content-Type', 'text/plain');
+  res.send(404, 'Don\'t be a dumbass.');
+});
 app.get('/', function(req, res) {
-  res.render('index');
+  res.render('index', {page: 'index'});
 });
 app.get('/:page', function(req, res) {
-  res.render(req.params['page']);
+  res.render(req.params['page'], {page: req.params['page']});
 });
 
 app.listen(process.env.PORT || 3000);
